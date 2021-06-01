0dbd7ae6778bd31abe26b88c98aa35cf3945226d
VaibhavGoyal14 <81407481+VaibhavGoyal14@users.noreply.github.com>
2021-06-01 10:53:13 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,44 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+
+     bg = ("255")
+
+    if(allContestants !== undefined){
+      fill("blue");
+      textSize(20);
+      text("*note: contestant who awnsered correct are highlighted in green color",130,230);
+    }
+
+
+  }
+
+}
